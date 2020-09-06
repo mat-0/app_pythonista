b73bac971c64740f24940b709e7533518c04846c
MatBenfield <MatBenfield@users.noreply.github.com>
2020-09-06 14:05:12 +0100

    tidying up
    Signed-off-by: MatBenfield <MatBenfield@users.noreply.github.com>

--- guessing_game.py
+++ guessing_game.py
@@ -1,18 +1,17 @@
 import random
 
-const min = 1
-const max = 10
-
-number = random.randint(min,max+1)
+MIN_VALUE = 1
+MAX_VALUE = 10
+number = random.randint(MIN_VALUE,MAX_VALUE)
 game = True
 tries = 0
 
 while game:
-    question = input(f"Pick an number between {min} and {max} ")
+    question = input(f"Pick an number between {MIN_VALUE} and {MAX_VALUE} ")
     tries += 1
     try:
         answer = int(question)
-        if  min <  answer > max:
+        if  MIN_VALUE <  answer > MAX_VALUE:
             print("out of range! please try again")
         elif answer == number:
             print(f"you got it! It took {tries} guesses")
